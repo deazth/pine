c77c82167dbc1634380ff0dafb3a9337e29cfbda
khvfizi <khafizi.amatnoh@gmail.con>
2019-12-29 21:54:37 +0800

    video background & dashboard

--- resources/views/vendor/backpack/base/inc/sidebar_content.blade.php
+++ resources/views/vendor/backpack/base/inc/sidebar_content.blade.php
@@ -1,4 +1,5 @@
 <!-- This file is used to store sidebar items, starting with Backpack\Base 0.9.0 -->
+
 <li class="nav-item"><a class="nav-link" href="{{ backpack_url('dashboard') }}"><i class="fa fa-dashboard nav-icon"></i> {{ trans('backpack::base.dashboard') }}</a></li>
 <li class="nav-item nav-dropdown"><a class="nav-link nav-dropdown-toggle" href="#"><i class="nav-icon fa fa-briefcase"></i> admin thingy</a>
   <ul class="nav-dropdown-items">
