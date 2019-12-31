4fdf966a0963031181eb4a0d6b94db86faf3465d
Mohd Afdzal Bin Kamal <afdzal3@gmail.com>
2019-12-31 09:32:14 +0800

    commot

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -13,5 +13,6 @@ class DatabaseSeeder extends Seeder
     {
         // $this->call(UsersTableSeeder::class);
         $this->call(SkillCatsTableSeeder::class);
+        $this->call(SkillsTableSeeder::class);
     }
 }
