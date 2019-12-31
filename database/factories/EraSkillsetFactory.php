0ca0b0490f25dd652a782c5f100ba0d6a0849764
Mohd Afdzal Bin Kamal <afdzal3@gmail.com>
2019-12-31 10:26:48 +0800

    commit

---
+++ database/factories/EraSkillsetFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\EraSkillset;
+use Faker\Generator as Faker;
+
+$factory->define(EraSkillset::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
